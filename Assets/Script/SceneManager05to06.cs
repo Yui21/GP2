3822726fede7f3f1f785ba1d05ba7b04bf7660be
Yui21 <84171257+Yui21@users.noreply.github.com>
2023-01-18 15:31:18 +0900

    0118

--- Assets/Script/SceneManager05to06.cs
+++ Assets/Script/SceneManager05to06.cs
@@ -6,15 +6,27 @@ using UnityEngine.SceneManagement;
 public class SceneManager05to06 : MonoBehaviour
 {
     private GameObject[] Item;
+    // private GameObject[] PictureBook;
 
     void Update()
     {
         Item = GameObject.FindGameObjectsWithTag("Item");
+    //     PictureBook = GameObject.FindGameObjectsWithTag("PictureBook");
 
-        if(Item.Length == 0)
+        
+    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    //private void OnControllerColliderHit(ControllerColliderHit other)
+    {
+        if (other.gameObject.CompareTag("Player") && Item.Length == 0)
         {
+            // Debug.Log("Hit");
             UnityEngine.SceneManagement.SceneManager.LoadScene("Scene06");
-        } 
+        }
+        //Debug.Log("Hit");
+        
     }
     
 }
