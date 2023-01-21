e4d8de142d08bc7b37be52d187c43076f063b0c7
Yui21 <84171257+Yui21@users.noreply.github.com>
2023-01-21 15:03:08 +0900

    リベ消える

--- Assets/Script/LibeFadeOut.cs
+++ Assets/Script/LibeFadeOut.cs
@@ -4,20 +4,8 @@ using UnityEngine;
 
 public class LibeFadeOut : MonoBehaviour
 {
-    private float nextTime;
-	public float interval = 1.0f;	// 点滅周期
-
-	// Use this for initialization
-	void Start() {
-		nextTime = Time.time;
-	}
-
-	// Update is called once per frame
-	void Update() {
-		if ( Time.time > nextTime ) {
-			GetComponent<SkinnedMeshRenderer>().enabled = !GetComponent<SkinnedMeshRenderer>().enabled;
-
-			nextTime += interval;
-		}
-	}
+    public void DeleteLibe()
+    {
+        this.gameObject.SetActive(false);
+    }
 }
